47b2c21be96485d923ce0f8bbaccaa17684f96f4
Jonathan Hrang <jsianguk@gmail.com>
2022-07-17 01:06:28 -0400

    search icon

--- lib/screens/sawnawk_screen.dart
+++ lib/screens/sawnawk_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:falamhymns/config/app_theme.dart';
 import 'package:falamhymns/models/sawnawk_model.dart';
 import 'package:falamhymns/screens/sub_screens/detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' as rootBundle;
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SawnawkScreen extends StatelessWidget {
   const SawnawkScreen({Key? key}) : super(key: key);
@@ -11,71 +13,90 @@ class SawnawkScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title: Text(
+            "Sawnawk Bible",
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+          actions: [
+            IconButton(
+              color: unselectedColor,
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                iconsPath + "search.svg",
+                color: selectedColor,
+              ),
+            ),
+          ],
+        ),
         body: FutureBuilder(
-      future: ReadJsonDate(),
-      builder: (context, data) {
-        if (data.hasError) {
-          return Center(child: Text("${data.error}"));
-        } else if (data.hasData) {
-          var items = data.data as List<SawnAwkModel>;
+          future: ReadJsonDate(),
+          builder: (context, data) {
+            if (data.hasError) {
+              return Center(child: Text("${data.error}"));
+            } else if (data.hasData) {
+              var items = data.data as List<SawnAwkModel>;
 
-          return ListView.builder(
-              itemCount: items == null ? 0 : items.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                  child: new InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailScreen(
-                                items[index].pageNumber,
-                                items[index].titleFalam),
-                          ));
-                    },
-                    child: Container(
-                      padding: EdgeInsets.only(bottom: 8),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+              return ListView.builder(
+                  itemCount: items == null ? 0 : items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                      child: new InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailScreen(
+                                    items[index].pageNumber,
+                                    items[index].titleFalam),
+                              ));
+                        },
+                        child: Container(
+                          padding: EdgeInsets.only(bottom: 8),
+                          child: Column(
                             children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: Text(
+                                        items[index].sawnawkNumber.toString(),
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )),
+                                  Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: Text(
+                                        items[index].titleFalam.toString(),
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      )),
+                                ],
+                              ),
                               Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Text(
-                                    items[index].sawnawkNumber.toString(),
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )),
-                              Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Text(
-                                    items[index].titleFalam.toString(),
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )),
+                                padding: EdgeInsets.all(8),
+                                child:
+                                    Text(items[index].titleEnglish.toString()),
+                              ),
                             ],
                           ),
-                          Padding(
-                            padding: EdgeInsets.all(8),
-                            child: Text(items[index].titleEnglish.toString()),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              });
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    ));
+                    );
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
 
